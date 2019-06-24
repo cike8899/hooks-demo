269403f73ccc5e7c1076f80f5bc67e9f28195b7f
assassin <assassin_cike@163.com>
2019-06-24 23:18:27 +0800

    add antd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Menu, Icon } from 'antd';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -7,30 +8,90 @@ import Users from './pages/Users';
 
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
+const { SubMenu } = Menu;
 
-        <Route path="/" exact component={Home} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  state = {
+    collapsed: false
+  };
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="app-wrapper">
+          <div style={{ width: 256 }}>
+            <Menu
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              mode="inline"
+              theme="dark"
+              inlineCollapsed={this.state.collapsed}
+            >
+              <Menu.Item key="1">
+                <Link to="/">
+                  <Icon type="pie-chart" />
+                  <span>Option 1</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/about">
+                  <Icon type="desktop" />
+                  <span>Option 2</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/users">
+                  <Icon type="inbox" />
+                  <span>Option 3</span>
+                </Link>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="mail" />
+                    <span>Navigation One</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <Menu.Item key="7">Option 7</Menu.Item>
+                <Menu.Item key="8">Option 8</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="appstore" />
+                    <span>Navigation Two</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="11">Option 11</Menu.Item>
+                  <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+            </Menu>
+          </div>
+          <div className="app-content">
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/users/" component={Users} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
